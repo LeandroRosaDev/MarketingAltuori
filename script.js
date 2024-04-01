88b19afd8c57a3cda97d48f77c1c397c5645195a
LeandroRosaDev <LeandroRosaDev>
2024-04-01 19:12:53 -0300

    Feat: Adicionando funcionalidade scroll to top

--- script.js
+++ script.js
@@ -77,3 +77,29 @@ navToSection.forEach(function (link) {
       (document.querySelector(".menuSidebar").style.display = "none");
   });
 });
+
+// AQUI É O JAVASCRIPT DO SCROLL TO TOP
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document smoothly
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
